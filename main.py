35bd80d930f75b2d74934a4d8a8f45710964f446
Janfiderheld <Janfitoe@web.de>
2025-07-28 16:17:35 +0200

    add main with CLI for db creation

--- main.py
+++
@@ -1,7 +0,0 @@
-from src.enums import ResourceType
-from src.rag.manager import RagDBManager
-
-if __name__ == "__main__":
-    recipe_rag = RagDBManager("Recipes", ResourceType.RECIPES)
-    wikihow_rag = RagDBManager("WikiHow", ResourceType.WIKIHOW)
-    cutting_rag = RagDBManager("CuttingVids", ResourceType.CUTTING_TUTORIALS)
