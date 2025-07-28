6a504143550828e2e91a135d8c882f1255c2ca29
Janfiderheld <Janfitoe@web.de>
2025-07-28 15:28:20 +0200

    remove food cutting qa evaluation

--- food_cutting_qa/prompting/prompter.py
+++
@@ -1,8 +0,0 @@
-class Prompter:
-    def __init__(self, model: str, temp=0.0, max_new_tokens=200):
-        self.model_name = model
-        self.temperature = temp
-        self.max_new_tokens = max_new_tokens
-
-    def prompt_model(self, system_msg: str, question: str) -> str:
-        pass
