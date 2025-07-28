40eb4343b66b051a6afbe4fc1927e8a2cee05163
Janfiderheld <Janfitoe@web.de>
2025-07-28 16:37:47 +0200

    fix error in CLI

--- applications/main_rag_creation.py
+++ applications/main_rag_creation.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     parser.add_argument("--resources", type=str, choices=["recipes", "wikihow", "cutting_vids", "all"], nargs="+", help="Choose for what resources the DBs should be created")
     args = parser.parse_args()
 
-    if "recipes" in args.tasks or "all" in args.tasks:
+    if "recipes" in args.resources or "all" in args.resources:
         recipe_rag = RagDBManager("Recipes", ResourceType.RECIPES)
         print("Created the RAG vector database for the recipe corpus")
-    if "wikihow" in args.tasks or "all" in args.tasks:
+    if "wikihow" in args.resources or "all" in args.resources:
         wikihow_rag = RagDBManager("WikiHow", ResourceType.WIKIHOW)
         print("Created the RAG vector database for the WikiHow copus")
-    if "cutting_vids" in args.tasks or "all" in args.tasks:
+    if "cutting_vids" in args.resources or "all" in args.resources:
         cutting_rag = RagDBManager("CuttingVids", ResourceType.CUTTING_TUTORIALS)
         print("Created the RAG vector database for the cutting tutorials")
